cd272be23bcbaf64db52c0050c597a437cfca5ec
William69420 <william.moller@elev.ga.lbs.se>
2023-02-06 09:56:37 +0100

    Refactored bullet.cs

--- Tank/Assets/bullet.cs
+++ Tank/Assets/bullet.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 public class bullet : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    public ParticleSystem BOOM;
-    public ParticleSystem Dirt_BOOM;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtExplisionParticle;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            ParticleSystem ex = Instantiate(BOOM, collision.GetContact(0).point, Quaternion.Euler(-90,0,0));
+            
+            ParticleSystem ex = Instantiate(explosionParticle, collision.GetContact(0).point, Quaternion.Euler(-90,0,0));
             Destroy(ex.gameObject, 2);
             Destroy(this.gameObject);
 
         }
         else if (collision.gameObject.tag == "Ground")
         {
-            ParticleSystem ex = Instantiate(Dirt_BOOM, collision.GetContact(0).point, Quaternion.Euler(-90, 0, 0));
+            ParticleSystem ex = Instantiate(dirtExplisionParticle, collision.GetContact(0).point, Quaternion.Euler(-90, 0, 0));
             Destroy(ex.gameObject, 2);
             Destroy(this.gameObject);
         }
